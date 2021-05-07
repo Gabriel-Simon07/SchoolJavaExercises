a320b98c83c6ffdf7bf0b6a3a4258dd4c3748a97
Gabriel Simon <gabrielsimon775@gmail.com>
2021-05-07 16:25:38 -0300

    Implementação dos testes do Boletim

--- src/main/java/br/com/proway/senior/escola/model/Boletim.java
+++ src/main/java/br/com/proway/senior/escola/model/Boletim.java
@@ -19,24 +19,26 @@ public class Boletim {
 	private ArrayList<Prova> provas;
 	
 	/**
-	 * O Boletim é criado apartir da
+	 * Cria um novo Boletim
+	 * 
+	 * O Boletim é criado a partir de um aluno e um período. Conforme as provas são adicionadas,
+	 * realiza-se o cálculo atualizado da média.
+	 * 
 	 * @param aluno
 	 * @param periodo
 	 * @param media
-	 * @param provas
 	 */
-	
-	
-	public Boletim(Aluno aluno, Integer periodo, Double media, ArrayList<Prova> provas) {
+	public Boletim(Aluno aluno, Integer periodo) {
 		super();
 		this.aluno = aluno;
 		this.periodo = periodo;
-		this.media = media;
+		this.media = 0.0;
 		this.provas = new ArrayList<Prova>();
 	}
 
 	/**
 	 * Aluno do boletim
+	 * 
 	 * @return
 	 */
 	public Aluno getAluno() {
@@ -45,17 +47,71 @@ public class Boletim {
 	
 	/**
 	 * Período do formato YYYYMM
+	 * 
 	 * @return
 	 */
 	public Integer getPeriodo() {
 		return periodo;
 	}
 	
+	/**
+	 * Retorna a média das provas atualizada.
+	 * 
+	 * @return
+	 */
 	public Double getMedia() {
 		return media;
 	}
+	
+	/**
+	 * Retorna todas as provas.
+	 * 
+	 * @return ArrayList<Prova>
+	 */
 	public ArrayList<Prova> getProvas() {
 		return provas;
 	}
+	
+	/**
+	 * Adiciona uma avaliação ao boletim.
+	 * 
+	 * Ao adicionar a Prova, a média é recalculada.
+	 * 
+	 * 
+	 * @param Prova
+	 */
+	public void addProva(Prova prova){
 
+	}
+	
+	/**
+	 * Remove uma avaliação do boletim.
+	 * 
+	 * Ao remover uma prova, a média é recalculada.
+	 * 
+	 * @return 
+	 */
+	public void removeProva(Prova prova) {
+		
+	}
+	
+	/**
+	 * Faz a média ponderada das provas.
+	 * 
+	 * Realiza o cálculo da média das notas das provas e atualiza o valor da média.
+	 * 
+	 */
+	public void calcularMedia() {
+		
+	}
+	
+	/**
+	 * Remove todas as provas
+	 * 
+	 * @return
+	 * 
+	 */
+	public void removeTodasProvas() {
+		this.provas = new ArrayList<Prova>();
+	}
 }
